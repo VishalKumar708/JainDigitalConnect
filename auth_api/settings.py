2b43df2c0acd1a18fb77d722c031803cbdc291b2
vishal-code123 <vk4499573@gmail.com>
2023-09-27 12:40:42 +0530

    DEBUG false and collect all STATIC_FILES

--- auth_api/settings.py
+++ auth_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a3z&h2hki16x=vk*87!8-xba!tg!50szf=!npl0=)kpvg(_4)w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.100.3', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -164,7 +164,7 @@ SENDER_NUMBER = '+15187540316'
 OTP_EXPIRY_DURATION = 600  # in seconds
 
 # *************************    Django Logger   ****************************
-import os
+# import os
 LOG_DIR = os.path.join(BASE_DIR, 'log')
 
 # Ensure the log directory exists, and create it if it doesn't
