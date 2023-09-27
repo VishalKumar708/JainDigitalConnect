2793911fa67e08e213d4053a560271213403b66e
vishal-code123 <vk4499573@gmail.com>
2023-09-27 16:03:38 +0530

    bug fix of custom manager in account app

--- accounts/manager.py
+++ accounts/manager.py
@@ -2,10 +2,11 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self,name, phoneNumber, dob, address, password=None):
+    def create_user(self,name,id, phoneNumber, dob, address, password=None):
         if not phoneNumber:
             raise ValueError("The Mobile Number field must be set")
         user = self.model(
+            id = id,
             phoneNumber=phoneNumber,
             dob=dob,
             address=address,
@@ -15,13 +16,14 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, name, userId, phoneNumber,dob, address,  password=None):
+    def create_superuser(self, name, id, phoneNumber,dob, address,  password=None):
         user = self.create_user(
+            id = id,
             phoneNumber=phoneNumber,
             password=password,
             dob=dob,
             address=address,
-            name=name
+            name=name,
         )
         user.is_admin = True
         user.is_staff = True
--- auth_api/settings.py
+++ auth_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a3z&h2hki16x=vk*87!8-xba!tg!50szf=!npl0=)kpvg(_4)w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
